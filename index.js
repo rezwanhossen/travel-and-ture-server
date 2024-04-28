861c2d821984b19cbdcb26835dbd0e7edf041962
rezwan-hossen <rezwanhossen625@gmail.com>
2024-04-28 16:19:56 +0600

    deleted tourespot

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/tourspot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await turistcolec.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
